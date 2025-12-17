fa93814c9bd3c772a6c574106edb30a756d8aa13
Thun (MacBook Pro) <bsthun@gmail.com>
2025-12-17 23:59:43 +0700

    Fix canvas error tracing

--- polygon/utility/canvas/canvas_import.go
+++ polygon/utility/canvas/canvas_import.go
@@ -1,7 +1,6 @@
 package canvas
 
 import (
-	"context"
 	"strings"
 
 	"go.scnd.dev/open/polygon/package/flow"
@@ -18,9 +17,8 @@ type ImportItem struct {
 
 func (r *Import) AddImport(item *ImportItem) error {
 	// * construct error dimension
-	dimension := flow.NewContext(context.TODO(), "canvas", make(map[string]any))
 	if item.Path == nil {
-		return flow.NewError(dimension, "path is nil", nil)
+		return flow.NewError(nil, "path is nil", nil)
 	}
 	if item.Alias == nil {
 		segments := strings.Split(*item.Path, "/")
@@ -28,8 +26,7 @@ func (r *Import) AddImport(item *ImportItem) error {
 	}
 	for _, existing := range r.Imports {
 		if *existing.Alias == *item.Alias {
-			dimension.Parameters["alias"] = *item.Alias
-			return flow.NewError(dimension, "import alias already exists", nil)
+			return flow.NewError(nil, "import alias already exists", nil)
 		}
 	}
 	r.Imports = append(r.Imports, item)
